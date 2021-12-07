ae1493614dc347647bfae7c9cecf4ae6eb1563d9
andreashsy <songyanghan+git@gmail.com>
2021-12-07 16:09:31 +0800

    added cookie class

---
+++ fcookieserver/src/main/java/fortunecookie/App.java
@@ -0,0 +1,24 @@
+package fortunecookie;
+
+import java.io.File;
+import java.io.IOException;
+
+public class App 
+{
+    public static void main( String[] args ) throws IOException{
+
+        //create cookie directory
+        String Wdir = System.getProperty("user.dir") + "\\cookie";
+        new File(Wdir).mkdirs();
+
+        Cookie cookie = new Cookie();
+
+        cookie.createCookie(Wdir);
+        String cL = cookie.loadFile(Wdir);
+        System.out.println(cL);
+
+        cookie.addCookie("newcookie", Wdir);
+
+
+    }
+}
---
+++ fcookieserver/src/main/java/fortunecookie/Cookie.java
@@ -0,0 +1,58 @@
+package fortunecookie;
+
+import java.io.*;
+import java.util.*;
+
+public class Cookie {
+    private String initialCookie;
+
+    public Cookie() {
+        this.initialCookie = "cookie1,cookie2,cookie3,cookie4";
+    }
+
+    public void createCookie(String dir) throws IOException {
+        File cookieFile = new File("" + dir + "\\cookie_file.txt");
+        cookieFile.createNewFile();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(dir + "\\cookie_file.txt"))) {
+            for (String item: initialCookie.split(",")) {
+                writer.write(item);
+                writer.newLine();
+            }
+            System.out.println("cookie_file created at " + dir);
+        }
+    }
+
+    public String loadFile(String dir) throws IOException {
+
+        try(BufferedReader br = new BufferedReader(new FileReader(dir + "\\cookie_file.txt"))) {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            String everything = sb.toString();
+            return everything;
+        }
+
+    }
+    
+    public String randomCookie(String cookieList) {
+
+        Random randomGenerator = new Random();
+        String[] cookies = cookieList.split("\\r?\\n");
+        int idx = randomGenerator.nextInt(cookies.length);
+        String randCookie = cookies[idx];
+        return randCookie;
+        
+    }
+
+    public void addCookie(String newCookie, String dir) throws IOException {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(dir  + "\\cookie_file.txt", true))) {
+            bw.write(newCookie);
+        }
+       
+    }
+}
